dac47f26776428913de7971a486716700bd69961
OĞUZHAN SEÇGEL <osecgel@hotmail.com>
2024-12-30 11:32:47 +0300

    initial

---
+++ src/main/java/com/oguzhansecgel/techcareer_backend/TechcareerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oguzhansecgel.techcareer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcareerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechcareerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oguzhansecgel/techcareer_backend/TechcareerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oguzhansecgel.techcareer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechcareerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
